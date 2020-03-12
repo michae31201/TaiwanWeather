1f7da80ad7e3a6e1ace308bd781b1497a22c43c7
michae31201 <michae31201@gmail.com>
2020-03-13 04:51:35 +0800

    將long 改為 lng

--- src/utils/fetchWeatherData.js
+++ src/utils/fetchWeatherData.js
@@ -1,12 +1,12 @@
-const fetchWeatherData = async (lat,long) => {
+const fetchWeatherData = async (lng,lat) => {
     const API_KEY = process.env.REACT_APP_API_KEY;
-    const URL = `/forecast/${API_KEY}/${lat},${long}/?lang=zh-tw&units=si`;
+    const URL = `/forecast/${API_KEY}/${lat},${lng}/?lang=zh-tw&units=si`;
     
     try {
         let response = await fetch(URL,{cache:'reload'});
         let jsonData =  await response.json();
 
-        return {...jsonData};
+        return jsonData;
     }catch(err){
         console.error(err);
     }
